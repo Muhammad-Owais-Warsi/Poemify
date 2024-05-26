5382b4ccfe63ff58b36c61cb74e22d91d629d805
Owais <warsimuhammadowais@gmail.com>
2024-05-26 13:33:07 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Hero from "@/components/hero";
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+      <div>
+          <Header/>
+          <Hero/>
+      </div>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Header() {
+    return (
+        <h1  className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug mt-4">Poemify</h1>
+    )
+}
