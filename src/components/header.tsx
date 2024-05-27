3aade704b14317721670dcb63f7ee9b62f39eeb9
Owais <warsimuhammadowais@gmail.com>
2024-05-27 12:26:26 +0530

    minor changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,6 @@
 
 export default function Header() {
     return (
-        <h1  className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug mt-4">Poemify</h1>
+        <h1  className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug mt-4 dark">Poemify</h1>
     )
 }
