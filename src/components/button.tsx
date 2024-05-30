8531ac8c257137ec1431de747db9ff9e38143bfb
Owais <warsimuhammadowais@gmail.com>
2024-05-30 14:54:39 +0530

    minor changes

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,13 +5,12 @@ import { toast, Toaster } from "sonner";
 import { useFormContext } from "@/context/formContext";
 import ImageBtn from "./imageBtn";
 
-
 type ButtonProp =  {
     isSubmit: boolean;
-    setIsSubmit:React.Dispatch<React.SetStateAction<boolean>>;
+    setIsSubmit: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function Button({isSubmit,setIsSubmit}: ButtonProp) {
+export default function Button({isSubmit, setIsSubmit}: ButtonProp) {
     const inputFileRef = useRef<HTMLInputElement>(null);
     const [imageDataURL, setImageDataURL] = useState<string | null>(null);
     const { updateForm } = useFormContext();
@@ -40,13 +39,11 @@ export default function Button({isSubmit,setIsSubmit}: ButtonProp) {
         }
     };
 
-
-
     return (
         <div className="flex flex-col justify-center items-center h-full w-full">
             <Toaster position="top-center" />
-            <div className="mx-auto w-full max-w-7xl">
-                <button className="relative p-1" onClick={openFileExplorer}>
+            <div className="mx-auto w-full max-w-7xl flex flex-col items-center">
+                <button className="relative p-1 uppercase mb-4" onClick={openFileExplorer}>
                     <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
                     <div className="px-4 py-2 bg-black rounded-[6px] relative text-white hover:bg-transparent transition duration-200">
                         Add Image +
@@ -60,7 +57,11 @@ export default function Button({isSubmit,setIsSubmit}: ButtonProp) {
                     onChange={handleFileSelection}
                 />
                 {/* Image preview */}
-                {imageDataURL && !isSubmit && <ImageBtn url={imageDataURL} />}
+                {imageDataURL && !isSubmit && (
+                    <div className="w-full mt-4">
+                        <ImageBtn url={imageDataURL} />
+                    </div>
+                )}
             </div>
         </div>
     );
