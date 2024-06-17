a0d0b260c563e67013bd79f9078908f1234b8190
shreyash3087 <shreyash3087@gmail.com>
2024-06-17 13:35:28 +0530

    Added Header And Footer Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import "./globals.css";
-
+import '@fortawesome/fontawesome-svg-core/styles.css';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,21 @@
 import Image from "next/image";
 import Hero from "@/components/hero";
 import Header from "@/components/header";
-import { Toaster} from 'sonner';
+import Footer from "@/components/footer";
+import { Toaster } from 'sonner';
 import { FormProvider } from "@/context/formContext";
 
 export default function Home() {
   return (
-      <FormProvider>
-         <Toaster richColors position="top-center" />
-          <Header/>
-          <Hero/>
-          
-      </FormProvider>
+      <div className="min-h-screen flex flex-col">
+        <FormProvider>
+           <Toaster richColors position="top-center" />
+            <Header/>
+            <div className="flex-grow flex items-center justify-center">
+              <Hero/>
+            </div>
+            <Footer/>
+        </FormProvider>
+      </div>
   );
 }
