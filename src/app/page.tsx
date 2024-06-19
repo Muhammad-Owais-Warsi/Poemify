d436103d115f9a43eff8ccc4c91ff9997677bb60
shreyash3087 <shreyash3087@gmail.com>
2024-06-19 19:12:23 +0530

    Added PreLoader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,75 @@
-'use client'
-
-import Image from "next/image";
+"use client"
+import React, { useEffect, useState } from "react";
+import Lottie from "react-lottie";
+import { Toaster } from 'sonner';
+import { FormProvider } from "@/context/formContext";
 import Hero from "@/components/hero";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import { Toaster } from 'sonner';
-import { FormProvider } from "@/context/formContext";
+import * as location from "./1055-world-locations.json";
+import * as success from "./1127-success.json";
 
-export default function Home() {
+const defaultOptions1 = {
+  loop: true,
+  autoplay: true,
+  animationData: location.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+const defaultOptions2 = {
+  loop: true,
+  autoplay: true,
+  animationData: success.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+function Spinner() {
+  const [loading, setLoading] = useState(false);
+  const [completed, setCompleted] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true); 
+      setTimeout(() => {
+        setCompleted(true); 
+      }, 1000);
+    }, 2000);
+  }, []);
+
   return (
-      <div className="min-h-screen flex flex-col">
-        <FormProvider>
-           <Toaster richColors position="top-center" />
-            <Header/>
+    <div className="min-h-screen flex flex-col  bg-black">
+      {!completed ? (
+        <div className="flex flex-grow justify-center items-center">
+          {!loading ? (
+            <Lottie
+              options={defaultOptions1}
+              width={"250px"}
+            />
+          ) : (
+            <Lottie
+              options={defaultOptions2}
+              width={"150px"}
+            />
+          )}
+        </div>
+      ) : (
+        <div className="flex flex-grow flex-col">
+          <FormProvider>
+            <Toaster richColors position="top-center" />
+            <Header />
             <div className="flex-grow flex items-center justify-center">
-              <Hero/>
+              <Hero />
             </div>
-            <Footer/>
-        </FormProvider>
-      </div>
+            <Footer />
+          </FormProvider>
+        </div>
+      )}
+    </div>
   );
 }
+
+export default Spinner;
